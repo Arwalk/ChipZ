94b41e74c994680f6b9a4c1f1871fd5f9075c464
Arwalk <siragentprovocateurarwalk@gmail.com>
2021-04-13 16:02:11 +0200

    first commit

---
+++ build.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard release options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+    const mode = b.standardReleaseOptions();
+
+    const lib = b.addSharedLibrary("chipz-lib", "src/lib/chipz.zig", .unversioned);
+
+    //const exe = b.addExecutable("chipz", "src/main.zig");
+    //exe.setTarget(target);
+    //exe.setBuildMode(mode);
+    //exe.install();
+
+    // const run_cmd = exe.run();
+    // run_cmd.step.dependOn(b.getInstallStep());
+    // if (b.args) |args| {
+    //     run_cmd.addArgs(args);
+    // }
+
+    const build_lib_step = b.step("build_lib", "Build the lib");
+    build_lib_step.dependOn(&lib.step);
+
+    //const run_step = b.step("run", "Run the app");
+    //run_step.dependOn(&run_cmd.step);
+}
---
+++ src/lib/chipz.zig
@@ -0,0 +1,3 @@
+const ChipZ = struct {
+    memory: [4096]u8
+};
